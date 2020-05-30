b88b67c70bc2d1ce8f0a0c16a6053b43f3401e4e
Antonio Dell'Annunziata <antonio.dell@gmx.de>
2020-05-30 12:32:31 +0200

    Prettier
    Signed-off-by: Antonio Dell'Annunziata <Antonio.Dell@gmx.de>

--- friend-info-service/src/main/kotlin/com/example/friendinfoservice/FriendController.kt
+++ friend-info-service/src/main/kotlin/com/example/friendinfoservice/FriendController.kt
@@ -50,6 +50,7 @@ class FriendController {
     @PatchMapping("/{id}", consumes = ["application/json-patch+json"])
     fun addFriend(@PathVariable("id") id: Long,
                   @Valid @RequestBody jsonPatch: JsonPatch): Mono<UserFriends> {
+
         return userFriendsRepository.findById(id)
                 .map {
                     val userFriendsJson = objectMapper.convertValue(it, JsonNode::class.java)
