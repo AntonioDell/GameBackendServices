885391cac6ccfdf9ece03b736208a047c1577d66
Antonio Dell'Annunziata <Antonio.Dell@gmx.de>
2020-06-20 09:59:52 +0200

    Remove deps from parent pom; Add eureka
    Signed-off-by: Antonio Dell'Annunziata <Antonio.Dell@gmx.de>

--- user-page-service/src/main/kotlin/dell/antonio/UserPageServiceApplication.kt
+++ user-page-service/src/main/kotlin/dell/antonio/UserPageServiceApplication.kt
@@ -2,16 +2,17 @@ package dell.antonio
 
 import org.springframework.boot.*
 import org.springframework.boot.autoconfigure.*
+import org.springframework.boot.autoconfigure.web.servlet.*
 import org.springframework.boot.context.properties.*
 import org.springframework.cloud.client.circuitbreaker.*
 import org.springframework.context.annotation.*
 import org.springframework.web.reactive.config.*
 import org.springframework.web.reactive.function.client.*
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [WebMvcAutoConfiguration::class])
+@ConfigurationPropertiesScan
 @EnableWebFlux
 @EnableCircuitBreaker
-@ConfigurationPropertiesScan
 class UserPageServiceApplication {
 
     @Bean
