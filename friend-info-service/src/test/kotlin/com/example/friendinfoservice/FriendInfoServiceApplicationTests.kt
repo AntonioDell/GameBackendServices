11b56db9c77c4f73ac0499134cbda17ddb1157ce
Antonio Dell'Annunziata <Antonio.Dell@gmx.de>
2020-06-22 00:19:39 +0200

    Fix dirty context issue
    In memory mongo db fails setup when context is dirty. Not sure why, but hotfix is to add @DirtiesContext to all tests.
    TODO investigate thoroughly
    
    Signed-off-by: Antonio Dell'Annunziata <Antonio.Dell@gmx.de>

--- friend-info-service/src/test/kotlin/com/example/friendinfoservice/FriendInfoServiceApplicationTests.kt
+++ friend-info-service/src/test/kotlin/com/example/friendinfoservice/FriendInfoServiceApplicationTests.kt
@@ -3,11 +3,13 @@ package com.example.friendinfoservice
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.*
 
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         classes = [FriendInfoServiceApplication::class])
 @AutoConfigureWebTestClient
+@DirtiesContext
 class FriendInfoServiceApplicationTests {
 
     @Test
